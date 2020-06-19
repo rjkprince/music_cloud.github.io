215a5b0637365302e60b6bfe5970733736831b0c
rjkprince <60301066+rjkprince@users.noreply.github.com>
2020-06-19 17:15:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,178 +1,272 @@
-var audio = new Audio();
-var playPause = document.getElementById('play-pause');
-var currentProgress = document.getElementById('current-progress');
-var duration = document.getElementById('duration');
-var songTitle = document.getElementById('song-title');
-var backgroundImg = document.getElementById('background-image');
-var songThumb = document.getElementById('player-thumb');
-var stepBack = document.getElementById('step-back');
-var stepForward = document.getElementById('step-forward');
-let xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'https://5ee2489c8b27f30016094881.mockapi.io/music', true);
-xhttp.send();
-xhttp.onreadystatechange = function () {
-  if (xhttp.readyState === 4) {
-    window.response = JSON.parse(this.responseText);
+$(function () {
+  audio = new Audio();
+  var playPause = $('#play-pause');
+  var currentProgress = $('#current-progress');
+  var duration = $('#duration');
+  var songTitle = $('#song-title');
+  var backgroundImg = $('#background-image');
+  var songThumb = $('#player-thumb');
+  var stepBack = $('#step-back');
+  var stepForward = $('#step-forward');
+  $.get('https://5ee2489c8b27f30016094881.mockapi.io/music', function (
+    response
+  ) {
+    window.response = response;
     window.currentSong = 0;
     window.currentThumb = response[0].thumbnail;
     window.currentUrl = response[0].url;
     window.currentTitle = response[0].title;
     audio.src = currentUrl;
-    songTitle.innerHTML = currentTitle;
-    backgroundImg.src = currentThumb;
-    songThumb.src = currentThumb;
-    stepForward.addEventListener('click', function () {
+
+    songTitle.text(currentTitle);
+    backgroundImg.attr('src', currentThumb);
+    songThumb.attr('src', currentThumb);
+    stepForward.click(function () {
       songForward();
     });
-    stepBack.addEventListener('click', function () {
-      songBackward();
+    stepBack.click(function () {
+      songForward();
+    });
+    generatePLaylist();
+  });
+
+  function songBackward() {
+    if (currentSong != 0) currentSong--;
+    else currentSong = response.length - 1;
+    var currentThumb = response[currentSong].thumbnail;
+    var currentUrl = response[currentSong].url;
+    var currentTitle = response[currentSong].title;
+    audio.src = currentUrl;
+
+    songTitle.text(currentTitle);
+    backgroundImg.attr('src', currentThumb);
+    songThumb.attr('src', currentThumb);
+
+    if (audio.paused) {
+      audio.play();
+      playPause.attr('src', './icons/pause-solid.svg');
+    } else {
+      audio.pause();
+      playPause.attr('src', './icons/play-solid.svg');
+    }
+  }
+
+  function songForward() {
+    if (currentSong < response.length - 1) currentSong++;
+    else currentSong = 0;
+    var currentThumb = response[currentSong].thumbnail;
+    var currentUrl = response[currentSong].url;
+    var currentTitle = response[currentSong].title;
+    audio.src = currentUrl;
+
+    songTitle.text(currentTitle);
+    backgroundImg.attr('src', currentThumb);
+    songThumb.attr('src', currentThumb);
+
+    if (audio.paused) {
+      audio.play();
+      playPause.attr('src', './icons/pause-solid.svg');
+    } else {
+      audio.pause();
+      playPause.attr('src', './icons/play-solid.svg');
+    }
+  }
+  playPause.click(function () {
+    if (audio.paused) {
+      audio.play();
+      playPause.attr('src', './icons/pause-solid.svg');
+    } else {
+      audio.pause();
+      playPause.attr('src', './icons/play-solid.svg');
+    }
+  });
+
+  function showDuration() {
+    audio.addEventListener('timeupdate', function () {
+      var playedPercentage = audio.currentTime / audio.duration;
+      playedPercentage = playedPercentage * 100;
+      currentProgress.css({ width: playedPercentage + '%' });
+
+      let currentTime = audio.currentTime;
+      let fullTime = audio.duration;
+      if (isNaN(fullTime)) fullTime = 0;
+      currentTime = Math.floor(currentTime);
+      fullTime = Math.floor(fullTime);
+
+      let currentMin = Math.floor(currentTime / 60);
+      let currentSec = Math.floor(currentTime - currentMin * 60);
+      let fullMin = Math.floor(fullTime / 60);
+      let fullSec = Math.floor(fullTime - fullMin * 60);
+      if (currentMin < 10) currentMin = '0' + currentMin;
+      if (currentSec < 10) currentSec = '0' + currentSec;
+      if (fullMin < 10) fullMin = '0' + fullMin;
+      if (fullSec < 10) fullSec = '0' + fullSec;
+      duration.text(
+        currentMin + ':' + currentSec + '/' + fullMin + ':' + fullSec
+      );
     });
   }
-};
+  showDuration();
+  ///////////////////////// Adding volume control
 
-function songBackward() {
-  if (currentSong != 0) currentSong--;
-  else currentSong = response.length - 1;
-  var currentThumb = response[currentSong].thumbnail;
-  var currentUrl = response[currentSong].url;
-  var currentTitle = response[currentSong].title;
-  audio.src = currentUrl;
-  songTitle.innerHTML = currentTitle;
-  backgroundImg.src = currentThumb;
-  songThumb.src = currentThumb;
-  if (audio.paused) {
-    audio.play();
-    playPause.src = './icons/pause-solid.svg';
-  } else {
-    audio.pause();
-    playPause.src = './icons/play-solid.svg';
-  }
-}
+  var speaker = $('#speaker');
+  speaker.click(function () {
+    if (audio.muted) {
+      audio.muted = false;
 
-function songForward() {
-  if (currentSong < response.length - 1) currentSong++;
-  else currentSong = 0;
-  var currentThumb = response[currentSong].thumbnail;
-  var currentUrl = response[currentSong].url;
-  var currentTitle = response[currentSong].title;
-  audio.src = currentUrl;
-  songTitle.innerHTML = currentTitle;
-  backgroundImg.src = currentThumb;
-  songThumb.src = currentThumb;
-  if (audio.paused) {
-    audio.play();
-    playPause.src = './icons/pause-solid.svg';
-  } else {
-    audio.pause();
-    playPause.src = './icons/play-solid.svg';
-  }
-}
-
-playPause.addEventListener('click', function () {
-  if (audio.paused) {
-    audio.play();
-    playPause.src = './icons/pause-solid.svg';
-  } else {
-    audio.pause();
-    playPause.src = './icons/play-solid.svg';
-  }
-});
-
-function showDuration() {
-  audio.addEventListener('timeupdate', function () {
-    var playedPercentage = audio.currentTime / audio.duration;
-    playedPercentage = playedPercentage * 100;
-
-    currentProgress.style.width = playedPercentage + '%';
-    let currentTime = audio.currentTime;
-    let fullTime = audio.duration;
-    if (isNaN(fullTime)) fullTime = 0;
-    currentTime = Math.floor(currentTime);
-    fullTime = Math.floor(fullTime);
-
-    let currentMin = Math.floor(currentTime / 60);
-    let currentSec = Math.floor(currentTime - currentMin * 60);
-    let fullMin = Math.floor(fullTime / 60);
-    let fullSec = Math.floor(fullTime - fullMin * 60);
-    if (currentMin < 10) currentMin = '0' + currentMin;
-    if (currentSec < 10) currentSec = '0' + currentSec;
-    if (fullMin < 10) fullMin = '0' + fullMin;
-    if (fullSec < 10) fullSec = '0' + fullSec;
-    duration.innerHTML =
-      currentMin + ':' + currentSec + '/' + fullMin + ':' + fullSec;
+      speaker.attr('src', './icons/volume-down-solid.svg');
+    } else {
+      audio.muted = true;
+      speaker.attr('src', './icons/volume-mute-solid.svg');
+    }
   });
-}
-showDuration();
-///////////////////////// Adding volume control
 
-var speaker = document.getElementById('speaker');
-speaker.addEventListener('click', function () {
-  if (audio.muted) {
-    audio.muted = false;
-    speaker.src = './icons/volume-down-solid.svg';
-  } else {
-    audio.muted = true;
-    speaker.src = './icons/volume-mute-solid.svg';
-  }
-});
-var volMin = document.getElementById('vol-min');
-volMin.addEventListener('click', function () {
-  if (audio.muted) {
-    audio.muted = false;
-    speaker.src = './icons/volume-down-solid.svg';
-  }
-  let currentVol = audio.volume;
-  currentVol = currentVol - 0.1;
-  audio.volume = currentVol;
-});
+  var volMin = $('#vol-min');
 
-var volPlus = document.getElementById('vol-plus');
-volPlus.addEventListener('click', function () {
-  if (audio.muted) {
-    audio.muted = false;
-    speaker.src = './icons/volume-down-solid.svg';
-  }
-  let currentVol = audio.volume;
-  currentVol = currentVol + 0.1;
-  audio.volume = currentVol;
-});
-
-//////// adding seekable functionality
-progressBar = document.getElementById('progress-bar');
-progressBar.addEventListener('click', function (e) {
-  let seekTo = e.offsetX / progressBar.offsetWidth;
-
-  audio.currentTime = seekTo * audio.duration;
-  currentProgress.style.width = seekTo * 100 + '%';
-});
-
-////////// audio ended then next
-audio.addEventListener('ended', function () {
-  songForward();
-});
-
-////// giving some click style for icons
-
-var icons = document.getElementsByClassName('icon');
-for (let i = 0; i < icons.length; i++) {
-  icons[i].addEventListener('click', function () {
-    icons[i].style.backgroundColor = '#00D2FF';
-    setTimeout(() => {
-      icons[i].style.backgroundColor = '#fff';
-    }, 100);
+  volMin.click(function () {
+    if (audio.volume > 0.1) {
+      if (audio.muted) {
+        audio.muted = false;
+        speaker.attr('src', './icons/volume-down-solid.svg');
+      }
+      let currentVol = audio.volume;
+      currentVol = currentVol - 0.1;
+      audio.volume = currentVol;
+    }
   });
-}
 
-//////designing volume bar
-var volBar = document.getElementById('vol-bar');
-var volCurrProgress = document.getElementById('vol-curr-progress');
-audio.addEventListener('volumechange', function () {
+  var volPlus = $('#vol-plus');
+  volPlus.click(function () {
+    if (audio.volume < 1) {
+      if (audio.muted) {
+        audio.muted = false;
+        speaker.attr('src', './icons/volume-down-solid.svg');
+      }
+      let currentVol = audio.volume;
+      currentVol = currentVol + 0.1;
+      audio.volume = currentVol;
+    }
+  });
+
+  //////// adding seekable functionality
+  progressBar = $('#progress-bar');
+  progressBar.click(function (e) {
+    let seekTo = e.offsetX / progressBar[0].offsetWidth;
+
+    audio.currentTime = seekTo * audio.duration;
+    currentProgress.css({ width: seekTo * 100 + '%' });
+  });
+
+  ////////// audio ended then next
+  audio.addEventListener('ended', function () {
+    songForward();
+  });
+
+  ////// giving some click style for icons
+
+  var icons = $('.icon');
+  for (let i = 0; i < icons.length; i++) {
+    icons[i].click(function () {
+      icons[i].style.backgroundColor = '#00D2FF';
+      setTimeout(() => {
+        icons[i].style.backgroundColor = '#fff';
+      }, 100);
+    });
+  }
+
+  //////designing volume bar
+  var volBar = $('#vol-bar');
+  var volCurrProgress = $('#vol-curr-progress');
+  audio.addEventListener('volumechange', function () {
+    let currentVol = audio.volume;
+    let volBarWidth = volBar[0].offsetWidth;
+
+    let myWidth = currentVol * volBarWidth;
+    volCurrProgress.css({ width: myWidth + 'px' });
+  });
+
   let currentVol = audio.volume;
   let volBarWidth = volBar.offsetWidth;
   let myWidth = currentVol * volBarWidth;
-  volCurrProgress.style.width = myWidth + 'px';
+  volCurrProgress.css({ width: myWidth + 'px' });
+  audio.pause();
+  playPause.attr('src', './icons/play-solid.svg'); //audio was playing as soon as it was loading so did this
+
+  /////////playlist arrow-style
+  $('#playlist').on({
+    mouseenter: function () {
+      $('#arrow').css({ transform: 'rotateZ(-180deg)' });
+    },
+    mouseleave: function () {
+      $('#arrow').css({ transform: 'rotateZ(0deg)' });
+    },
+  });
+
+  ///////////generating playlist cards
+  function generatePLaylist() {
+    //   <div class="playlist-item">
+    //   <h1 class="song-title">Attention</h1>
+    //   <p class="song-duration">00:00</p>
+    // </div>
+    for (let i = 0; i < response.length; i++) {
+      let playItem = $('<div>').addClass('playlist-item');
+      let title = $('<h1>').addClass('song-title').text(response[i].title);
+      let thumbnail = $('<img>')
+        .addClass('playlist-thumb')
+        .attr('src', response[i].thumbnail);
+      playItem.append(title, thumbnail);
+      playItem.attr('id', response[i].id);
+      if (currentSong == i) {
+        playItem.css({
+          'background-color': '#d1e079',
+        });
+      }
+      playItem.click(function () {
+        currentSong = i;
+        var currentThumb = response[currentSong].thumbnail;
+        var currentUrl = response[currentSong].url;
+        var currentTitle = response[currentSong].title;
+        audio.src = currentUrl;
+        $('.playlist-item').css({
+          'background-color': 'rgba(255, 255, 255, 0.1)',
+        });
+        playItem.css({ 'background-color': '#d1e079' });
+
+        songTitle.text(currentTitle);
+        backgroundImg.attr('src', currentThumb);
+        songThumb.attr('src', currentThumb);
+        audio.play();
+        playPause.attr('src', './icons/pause-solid.svg');
+      });
+      $('#playlist-items').append(playItem);
+    }
+  }
+
+  /////////////////sending new audio to backend
+  function dataSend(data) {
+    $.post(
+      'https://5ee2489c8b27f30016094881.mockapi.io/music',
+      data,
+      function () {
+        location.assign('./index.html');
+      }
+    );
+  }
+  $('#btn-send').click(function () {
+    if (
+      $('.song-input')[1].value != null &&
+      $('.song-input')[1].value != '' &&
+      $('.song-input')[0].value != null &&
+      $('.song-input')[0].value != '' &&
+      $('.song-input')[2].value != '' &&
+      $('.song-input')[1].value != null
+    ) {
+      let data = {
+        title: $('.song-input')[0].value,
+        thumbnail: $('.song-input')[1].value,
+        url: $('.song-input')[2].value,
+      };
+      dataSend(data);
+    }
+  });
 });
-let currentVol = audio.volume;
-let volBarWidth = volBar.offsetWidth;
-let myWidth = currentVol * volBarWidth;
-volCurrProgress.style.width = myWidth + 'px';
